32d8fa768f07d9c1e473ca91f3f4fa440df94f00
prismism-git <brad.mcalpine@hotmail.com>
2017-07-30 02:49:41 +1000

    More
    Pushing entire project folder to see if this helps with node editor issues

---
+++ Assets/Character Scripts/init.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenuAttribute(fileName="Mood Maker", menuName="Mood Maker/Interface Demo")]
+	public class Init : ScriptableObject {
+
+		public GameObject prefab;
+		public float volume;
+		public string fileName;
+		public float bass;
+		public float mids;
+		public float treble;
+}
